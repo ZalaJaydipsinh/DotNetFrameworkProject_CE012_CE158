808a3df22b2b119fd6ab816a7d4dfee885d9828c
Jaydipsinh Zala <jbzala004@gmail.com>
2022-09-27 23:55:44 +0530

    SQL queries for all tables are added.

--- SQLQuery1.sql
+++ SQLQuery1.sql
@@ -1,7 +1,31 @@
 ﻿CREATE TABLE [dbo].[Newuser] (
-	[Uid]	INT IDENTITY(1,1) PRIMARY KEY,
+    [Uid]      INT            IDENTITY (1, 1) NOT NULL,
     [UserName] NVARCHAR (60)  NOT NULL,
-    [Email]    NVARCHAR (200) UNIQUE NOT NULL,
+    [Email]    NVARCHAR (200) NOT NULL,
     [Contact]  NCHAR (20)     NOT NULL,
     [Password] NCHAR (50)     NOT NULL,
+    PRIMARY KEY CLUSTERED ([Uid] ASC),
+    UNIQUE NONCLUSTERED ([Email] ASC)
+);
+
+CREATE TABLE [dbo].[Category] (
+    [Id]   INT            IDENTITY (1, 1) NOT NULL,
+    [Uid]  INT            NOT NULL,
+    [Name] NVARCHAR (256) NOT NULL,
+    PRIMARY KEY CLUSTERED ([Id] ASC),
+    CONSTRAINT [FK_UserId] FOREIGN KEY ([Uid]) REFERENCES [dbo].[Newuser] ([Uid])
+);
+
+CREATE TABLE [dbo].[Urls] (
+    [Id]          INT             IDENTITY (1, 1) NOT NULL,
+    [Uid]         INT             NOT NULL,
+    [Cid]         INT             NULL,
+    [Link]        NVARCHAR (500)  NOT NULL,
+    [Name]        NVARCHAR (500)  NOT NULL,
+    [Description] NVARCHAR (2000) NULL,
+    [Date]        DATE            DEFAULT (getdate()) NULL,
+    [IsPublic]    BIT             NOT NULL,
+    PRIMARY KEY CLUSTERED ([Id] ASC),
+    CONSTRAINT [FK_UserIdUrls] FOREIGN KEY ([Uid]) REFERENCES [dbo].[Newuser] ([Uid]),
+    CONSTRAINT [FK_CatIdUrls] FOREIGN KEY ([Cid]) REFERENCES [dbo].[Category] ([Id])
 );
