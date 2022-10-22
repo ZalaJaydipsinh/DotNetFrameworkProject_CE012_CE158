8b7d7483356cef97e532521cebd362c933ba4cdf
ZalaJaydipsinh <64979418+ZalaJaydipsinh@users.noreply.github.com>
2022-10-22 11:00:11 +0530

    Update SQLQuery1.sql

--- SQLQuery1.sql
+++ SQLQuery1.sql
@@ -1,4 +1,4 @@
-﻿CREATE TABLE [dbo].[Newuser] (
+CREATE TABLE [dbo].[Newuser] (
     [Uid]      INT            IDENTITY (1, 1) NOT NULL,
     [UserName] NVARCHAR (60)  NOT NULL,
     [Email]    NVARCHAR (200) NOT NULL,
@@ -10,15 +10,15 @@
 
 CREATE TABLE [dbo].[Category] (
     [Id]   INT            IDENTITY (1, 1) NOT NULL,
-    [Uid]  INT            NOT NULL,
+    [Uid]  INT            NULL,
     [Name] NVARCHAR (256) NOT NULL,
     PRIMARY KEY CLUSTERED ([Id] ASC),
-    CONSTRAINT [FK_UserId] FOREIGN KEY ([Uid]) REFERENCES [dbo].[Newuser] ([Uid])
+    CONSTRAINT [FK_UserId] FOREIGN KEY ([Uid]) REFERENCES [dbo].[Newuser] ([Uid]) ON DELETE SET NULL
 );
 
 CREATE TABLE [dbo].[Urls] (
     [Id]          INT             IDENTITY (1, 1) NOT NULL,
-    [Uid]         INT             NOT NULL,
+    [Uid]         INT             NULL,
     [Cid]         INT             NULL,
     [Link]        NVARCHAR (500)  NOT NULL,
     [Name]        NVARCHAR (500)  NOT NULL,
@@ -26,6 +26,6 @@ CREATE TABLE [dbo].[Urls] (
     [Date]        DATE            DEFAULT (getdate()) NULL,
     [IsPublic]    BIT             NOT NULL,
     PRIMARY KEY CLUSTERED ([Id] ASC),
-    CONSTRAINT [FK_UserIdUrls] FOREIGN KEY ([Uid]) REFERENCES [dbo].[Newuser] ([Uid]),
-    CONSTRAINT [FK_CatIdUrls] FOREIGN KEY ([Cid]) REFERENCES [dbo].[Category] ([Id])
+    CONSTRAINT [FK_UserIdUrls] FOREIGN KEY ([Uid]) REFERENCES [dbo].[Newuser] ([Uid]) ON DELETE CASCADE,
+    CONSTRAINT [FK_CatIdUrls] FOREIGN KEY ([Cid]) REFERENCES [dbo].[Category] ([Id]) ON DELETE SET NULL
 );
